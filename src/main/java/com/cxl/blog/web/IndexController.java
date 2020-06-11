375cf93d65f3be0ebfe1fb73c0313de953dd78b7
caixinglong <caixinglong@xigua.city>
2020-06-11 13:31:56 +0800

    add exception model

---
+++ src/main/java/com/cxl/blog/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.cxl.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        int i =9/0;     //主动注入500错误异常
+        return "index";
+    }
+}
