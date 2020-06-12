d1f40d12be871167a5c6a0dba86bb89542c690cb
caixinglong <caixinglong@xigua.city>
2020-06-12 21:55:10 +0800

    add search

--- src/main/java/com/cxl/blog/web/IndexController.java
+++ src/main/java/com/cxl/blog/web/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by limi on 2017/10/13.
@@ -34,6 +35,15 @@ public class IndexController {
         model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
         return "index";
+    }
+
+
+    @GetMapping("/search")
+    public String search(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                         @RequestParam String query, Model model) {
+        model.addAttribute("page", blogService.listBlog("%"+query+"%", pageable));
+        model.addAttribute("query", query);
+        return "search";
     }
 
     @GetMapping("/blog/{id}")
